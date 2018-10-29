67bcbf254772a19f9a11157153d4c9d330fd28cb
Quang Nguyen <qnguyen3411@gmail.com>
2018-10-29 16:10:01 -0700

    peer drawing implemented

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
-const fs = require('fs');
 const express = require('express');
-const mysql = require('mysql');
-const JWT = require('jsonwebtoken');
-const { JWT_SECRET, dbConfig } = require('./configurations')
-const RoomManager = require('./roomManager')
+const findKey = require('lodash/findKey');
 
 const app = express()
 const server = app.listen(5000, () => {
   console.log("LISTENING")
 })
-
-const connection = mysql.createConnection(dbConfig);
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
 
 
 const io = require('socket.io')(server);
@@ -27,144 +14,47 @@ let rooms = {};
 
 io.on('connection', function (socket) {
 
-  const socketId = socket.client.id;
-  let roomKey;
+  console.log("SOCKET CONNECTED")
+  let currRoom;
 
-  
-    // Joining/ Disconnecting logic
+  socket.on('join', function(data) {
+    // Authenticate join info
 
-  // Side effects: roomKey
-  // Captured: connection
-  socket.on('join', async data => {
-    const roomId = data.roomId;
-    roomKey = `${roomId}`;
-    try {
-      // Database interaction
-      const { created_at : createdAt ,  expires_at: expiresAt } = await checkRoomStatus(connection, roomId);
-      if (new Date().getTime() > expiresAt) {
-        throw new Error("Room already expired on " + new Date(expiresAt));
-      }
-
-      let username;
-      let id;
-      const tokenData = await decodeToken(data.token);
-      if (tokenData) {
-        username = tokenData.sub.username;
-        id = tokenData.sub.id;
-        // Database interaction
-        await recordJoinEvent(connection, id, roomId);
-      }
-      username = username || "Guest";
-
-      if (!rooms[roomKey]) {
-        const newRoom = new RoomManager(roomKey, createdAt, expiresAt);
-        newRoom.onNewToken = () => { io.to(roomKey).emit('newToken'); };
-        newRoom.onExpire = () => { io.to(roomKey).emit('roomExpired'); }
-        newRoom.host = socketId;
-        socket.emit('setAsHost');
-        rooms[roomKey] = newRoom;
-      }
-
-      if (rooms[roomKey].roomIsEmpty()) {
-        const strokeLogPath = __dirname + `/../strokeLogs/${roomKey}.txt`;
-        fs.readFile(strokeLogPath, 'utf8', (err, data) => {
-          socket.emit('strokeLog', { data });
-        })
-      }
-
-      socket.emit('roomInfo', rooms[roomKey].getRoomInfo());
-      socket.broadcast.to(roomKey)
-        .emit('userJoined', { id: socketId, username });
-
-      socket.join(roomKey);
-      rooms[roomKey].addUser(socketId, username)
-      socket.emit('setUsername', { username });
-
-    } catch (error) {
-      console.error(error);
-      socket.emit('roomUnavailable');
-      socket.disconnect();
-      return;
-    }
-  })
-
-  function checkRoomStatus(connection, roomId) {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'SELECT created_at, expires_at, is_active FROM rooms WHERE id = ?',
-        [roomId],
-        function (error, results, fields) {
-          if (error) { reject(error) };
-          if (!results) { resolve(false) };
-          resolve(results[0]);
-        })
-    });
-  }
-
-  function decodeToken(token) {
-    // Decode token wirhout catching
-    return new Promise(function (resolve, reject) {
-      JWT.verify(token, JWT_SECRET, function (err, decoded) {
-        if (err) resolve(null);
-        resolve(decoded);
-      })
-    })
-  }
-
-  function recordJoinEvent(connection, userId, roomId) {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'INSERT INTO users_rooms (room_id, user_id, created_at, updated_at)'
-        + ' VALUES (?,?, NOW(), NOW())'
-        + ' ON DUPLICATE KEY UPDATE updated_at = NOW()',
-        [roomId, userId],
-        function (error, results) {
-          if (error) { reject(error) }
-          resolve(results);
-        })
-    })
-  }
-
-
-  socket.on('canvasShare', data => {
-    io.to(data.target).emit('canvasData', { data: data.dataURI });
-  })
-
-  socket.on('exitClicked', () => {
-    socket.disconnect();
+    // Get username, roomId
+    const username = "bob";
+    currRoom = data.room;
+    socket.broadcast.to(currRoom).emit('peerJoined', { username, id: socket.client.id });
+    socket.join(currRoom);
   })
 
   socket.on('disconnect', () => {
-    socket.broadcast.to(roomKey).emit('userDisconnected', { id: socketId });
-    if (!rooms[roomKey]) { return };
-    rooms[roomKey].removeUser(socketId);
-    io.to(rooms[roomKey].host).emit('setAsHost');
+    console.log("SOCKET DISCONNECTED")
+    // console.log(socket.disconnected);
   })
 
-
-  // Drawing logic
-
-  socket.on('strokeStart', data => {
-    const emitData = { id: socketId, strokeData: data }
-    socket.broadcast.to(roomKey).emit('otherStrokeStart', emitData)
+  socket.on('cursorSizeUpdate',({data}) => {
+    console.log("CURSOR SIZE UPDATE")
+    console.log(data)
+    socket.broadcast.to(currRoom).emit('peersCursorSizeUpdate', { id: socket.client.id, data });
   })
 
-  socket.on('strokeUpdate', data => {
-    const emitData = { id: socketId, newPoints: data };
-    socket.broadcast.to(roomKey).emit('otherStrokeUpdate', emitData)
+  socket.on('mousePosUpdate',({data}) => {
+    socket.broadcast.to(currRoom).emit('peersMousePosUpdate', { id: socket.client.id, data });
   })
 
-  socket.on('strokeEnd', data => {
-    rooms[roomKey].record(data.strokeData);
-    const emitData = { id: socketId, newPoints: data.newPoints }
-    socket.broadcast.to(roomKey).emit('otherStrokeEnd', emitData)
-  })
+  socket.on('canvasActionStart',({data}) => {
+    socket.broadcast.to(currRoom).emit('peersCanvasActionStart', { id: socket.client.id, data });
+  }) 
 
-  socket.on('cursorMovedOnCanvas', data => {
-    const emitData = { id: socketId, newPoint: data }
-    socket.broadcast.to(roomKey).emit('otherCursorMovedOnCanvas', emitData);
-  })
+  socket.on('canvasActionEnd',({data}) => {
+    console.log("CANVAS ACTION END SIGNAL")
+    socket.broadcast.to(currRoom).emit('peersCanvasActionEnd', { id: socket.client.id, data });
+  }) 
 
 })
+
+// function getRoom(socket) {
+//   return findKey(socket.rooms, (roomName) => roomName !== socket.client.id )
+// }
 
 
