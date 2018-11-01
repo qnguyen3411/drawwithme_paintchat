a446758377cfcc9370a400de6d3d4525a25b35fb
Quang Nguyen <qnguyen3411@gmail.com>
2018-10-31 23:59:23 -0700

    roominfo and token system working

--- hub_api/index.js
+++ hub_api/index.js
@@ -1,8 +1,9 @@
-const config =  require('./config')
-module.exports =  {
-  
-  baseUrl : config.root,
-  
+const config = require('./config')
+const axios = require('axios');
+const instance = axios.create({ baseURL: 'http://localhost:8000' })
+
+module.exports = {
+
 
   // Writing operations
 
@@ -10,19 +11,37 @@ module.exports =  {
 
   },
 
-  consumeTimeToken(timeToken) {
-
+  consumeTimeToken(roomId) {
+    return instance.get(`/rooms/token/${roomId}`)
+      .then(({ status, data }) => {
+        if (status !== 200) { return Promise.reject('Server response error'); }
+        if (data.status === 'error') { return Promise.reject('Token consumption error') }
+        return data.data;
+      })
   },
 
   // Reading operations
 
-  getRoomInfo(roomId) {
-
+  async getRoomInfo(roomId) {
+    return instance.get('/rooms/' + roomId)
+      .then(({ status, data }) => {
+        if (status !== 200) { return Promise.reject('Server response error'); }
+        if (!data.data) { return Promise.reject('Room not found error'); }
+        return data.data;
+      })
   },
 
   // Return username & id if valid, null if not
   getUserIdentity(token) {
-
+    if (!token) return Promise.resolve(null);
+    return instance({
+      method: 'get',
+      url: '/users/verify',
+      headers: { 'Authorization': token }
+    }).then(({ status, data }) => {
+      if (status !== 200) { return Promise.reject('Server response error'); }
+      return data.data;
+    })
   },
 
 
--- server.js
+++ server.js
@@ -14,71 +14,107 @@ let rooms = {};
 
 io.on('connection', function (socket) {
 
-  console.log("SOCKET CONNECTED")
   let currRoom;
 
-  socket.on('join', function(data) {
-    // Authenticate join info
-    const roomId = data.room;
-    const room = hubServer.getRoomInfo();
-    if (!room) {
-      forceDisconnect();
+  socket.on('join', async function (data) {
+    try {
+      // Authenticate join info
+      const roomId = data.room;
+      const room = await hubServer.getRoomInfo(roomId);
+      if (!room) { throw new Error('Room not found') }
+
+      socket.emit('roomInfo', room);
+
+      currRoom = roomId;
+      rooms[roomId] = rooms[roomId] || {};
+      // TODO: maybe is possible to have another socket give userlist to us
+      socket.emit('userList', rooms[roomId]);
+      let user = await hubServer.getUserIdentity(data.token);
+      if (user) {
+        // TODO: implement recordJoin
+        hubServer.recordJoin(user.id, roomId);
+      } else {
+        user = {username: getAnonName()};
+      }
+
+
+      rooms[roomId][socket.client.id] = { username: user.username };
+      socket.emit('assignedUsername', { username: user.username });
+      socket.broadcast.to(currRoom).emit('peerJoined', {
+         username: user.username, id: socket.client.id 
+        });
+
+      socket.join(currRoom);
+      
+    } catch (err) {
+      forceDisconnect()
+      console.log(err)
     }
-    socket.emit('roomInfo', room);
-    
-    currRoom = roomId;
-    rooms[roomId] = rooms[roomId] || {};
-    console.log("ROOM: ")
-    console.log(rooms[roomId])
-    socket.emit('userList', rooms[roomId]);
-    // Get username, roomId
-    let user = hubServer.getUserIdentity(data.token);
-    if (user) {
-      hubServer.recordJoin(user.id, roomId);
-    } else {
-      user = {username: getAnonName()};
-    }
-    rooms[roomId][socket.client.id] = { username: user.username };
-    socket.broadcast.to(currRoom).emit('peerJoined', { username: user.username, id: socket.client.id });
-    socket.join(currRoom);
   })
 
+
+
   socket.on('disconnect', () => {
-    console.log("Socket disconnected")
+    socket.broadcast.to(currRoom).emit('peerLeft', { id: socket.client.id });
     if (rooms[currRoom] && rooms[currRoom][socket.client.id]) {
       delete rooms[currRoom][socket.client.id];
     }
   })
 
+  socket.on('tokenConsumed', () => {
+    hubServer.consumeTimeToken(currRoom)
+      .then(data => {
+        io.to(currRoom).emit('tokenConsumed', data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 
-  socket.on('cursorSizeUpdate',({data}) => {
+
+  socket.on('cursorSizeUpdate', ({ data }) => {
     socket.broadcast.to(currRoom).emit('peersCursorSizeUpdate', { id: socket.client.id, data });
   })
 
-  socket.on('mousePosUpdate',({data}) => {
+  socket.on('mousePosUpdate', ({ data }) => {
     socket.broadcast.to(currRoom).emit('peersMousePosUpdate', { id: socket.client.id, data });
   })
-
-  socket.on('canvasActionStart',({data}) => {
+  socket.on('canvasActionStart', ({ data }) => {
     socket.broadcast.to(currRoom).emit('peersCanvasActionStart', { id: socket.client.id, data });
-  }) 
-
-  socket.on('canvasActionEnd',({data}) => {
+  })
+  socket.on('canvasActionEnd', ({ data }) => {
     socket.broadcast.to(currRoom).emit('peersCanvasActionEnd', { id: socket.client.id, data });
-  }) 
+  })
+
+
+  socket.on('chatMessageSent', ({ data }) => {
+    socket.broadcast.to(currRoom).emit('peersChatMessageSent', { id: socket.client.id, data });
+  })
+
 
   function forceDisconnect() {
-
+    socket.emit('forceDisconnect');
+    socket.disconnect();
   }
 
   function getAnonName() {
-    return "Anon_" + Math.random().toString(36).substring(7);
+    return "anon_" + Math.random().toString(36).substring(7);
   }
 
 })
 
-// function getRoom(socket) {
-//   return findKey(socket.rooms, (roomName) => roomName !== socket.client.id )
-// }
+
+// On join:
+// part a
+// authenticate room
+// identify user
+
+// part b
+// Pick random client from room
+// request canvas data
+// or fetch strokelog?
+// request user list
 
 
+// part c
+// attach all other event listeners
